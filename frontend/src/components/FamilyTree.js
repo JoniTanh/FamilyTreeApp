9f6dc44c02e62665b9944189e2de44cf2898fdf6
Joni Tanhuansuu <jonitanhuansuu91@gmail.com>
2023-02-17 23:16:34 +0200

    frame for familytree graph and person view/page

--- frontend/src/components/FamilyTree.js
+++ frontend/src/components/FamilyTree.js
@@ -1,8 +1,112 @@
-const FamilyTree = () => (
-  <div>
-    <h1>Sukupuu</h1>
-    <div>Tähän on tulossa interaktiivinen sukupuukaavio</div>
-  </div>
-);
+import "../assets/familyTree.css";
+import React from "react";
+import dTree from "d3-dtree";
+import _ from "lodash";
+import * as d3 from "d3";
+window.d3 = d3;
+
+class FamilyTree extends React.Component {
+  componentDidMount() {
+    let treeData = [
+      {
+        name: "Isovaari",
+        class: "man",
+        textClass: "emphasis",
+        marriages: [
+          {
+            spouse: {
+              name: "Isomummo",
+              class: "woman",
+              extra: {
+                nickname: "Illi",
+              },
+            },
+            children: [
+              {
+                name: "Isä",
+                class: "man",
+                marriages: [
+                  {
+                    spouse: {
+                      name: "Äiti",
+                      class: "woman",
+                    },
+                    children: [
+                      {
+                        name: "Poika",
+                        class: "man",
+                        marriages: [
+                          {
+                            spouse: {
+                              name: "Tyttöystävä",
+                              class: "woman",
+                            },
+                          },
+                        ],
+                      },
+                      {
+                        name: "Tyttö",
+                        class: "woman",
+                      },
+                      {
+                        name: "Poika",
+                        class: "man",
+                      },
+                      {
+                        name: "Tyttö",
+                        class: "woman",
+                      },
+                      {
+                        name: "Tyttö",
+                        class: "woman",
+                      },
+                      {
+                        name: "Tyttö",
+                        class: "woman",
+                      },
+                    ],
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+    ];
+
+    dTree.init(treeData, {
+      target: "#graph",
+      height: 600,
+      width: 1400,
+      debug: true,
+      textRenderer: function (name, textClass) {
+        return (
+          "<p align='center' className='" + textClass + "'>" + name + "</p>"
+        );
+      },
+      nodeRenderer: function (id, textRenderer, name, textClass, nodeClass) {
+        let node = "";
+        node += "<div ";
+        node += 'style="height:100%;width:100%;" ';
+        node += 'className="' + nodeClass + '" ';
+        node += 'id="node' + id + '">\n';
+        node += textRenderer(name, textClass);
+        node += "</div>";
+        return node;
+      },
+    });
+  }
+
+  render() {
+    return (
+      <>
+        <div className="familyTreesvg">
+          <h1 style={{ textAlign: "center" }}>Henkilön xx Sukupuu</h1>
+          <div id="graph"></div>
+        </div>
+      </>
+    );
+  }
+}
 
 export default FamilyTree;
--- frontend/src/components/Person.js
+++ frontend/src/components/Person.js
@@ -4,13 +4,65 @@ const Person = () => {
   const { state } = useLocation();
 
   return (
-    <div>
-      <h1>Henkilö</h1>
-      <ul>
-        <li>{state.firstName}</li>
-        <li>{state.lastName}</li>
-      </ul>
-    </div>
+    <>
+      <div className="pt-5 pb-3 px-5 d-flex justify-content-center">
+        <div className="pt-4 pb-4 px-5 w-75 shadow bg-white rounded border border-info">
+          <div>
+            <h1>
+              {state.firstName} {state.lastName}
+            </h1>
+            <div>
+              <b>kutsumanimi:</b> {state.firstName}
+            </div>
+            <div className="pb-3">
+              <b>sukunimi:</b> {state.lastName}
+            </div>
+            <div>
+              <b>syntynyt (paikka, aika):</b> {state.birthPlace}{" "}
+              {state.birthTime}
+            </div>
+            <div className="pb-3">
+              <b>kummit ja kastepäivä:</b> {state.godparents}
+            </div>
+            <div>
+              <b>kuollut (paikka, aika, kuolinsyy):</b> {state.deathPlace}{" "}
+              {state.deathTime} {state.deathReason}
+            </div>
+            <div className="pb-3">
+              <b>hautauspaikka- ja aika:</b> {state.burialPlot}{" "}
+              {state.burialTime}
+            </div>
+            <div className="pb-3">
+              <b>pienoiselämänkerta:</b> {state.lifeStory}
+            </div>
+            <div>
+              <b>lähteet:</b> {state.sources}
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="px-5 d-flex justify-content-center">
+        <div className="w-75">
+          <div>
+            <span>
+              <button type="button" className="btn btn-info">
+                Tulosta
+              </button>
+            </span>
+            <span className="px-2">
+              <button type="button" className="btn btn-warning">
+                Muokkaa
+              </button>
+            </span>
+            <span>
+              <button type="button" className="btn btn-danger">
+                Peruuta
+              </button>
+            </span>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
